31aa2e88159963e0688bc73784516e5dd49672ad
Adam Orczyk <adam.orczyk@gmail.com>
2022-09-26 13:32:59 +0200

    Update pxt.json, main.ts

--- main.ts
+++ main.ts
@@ -1,67 +1,120 @@
-let signal2: number[] = [9124, 4514, 586, 558, 585, 558, 586, 557, 587, 1674, 585, 1674, 586, 1674, 586, 1675, 585, 559, 586, 1676, 586, 1674, 585, 1674, 587, 558, 586, 557, 587, 556, 586, 558, 586, 1674, 585, 1676, 585, 1676, 584, 1674, 586, 1673, 585, 559, 585, 1674, 584, 559, 586, 557, 585, 558, 585, 558, 586, 557, 586, 557, 585, 1673, 586, 557, 585, 1674, 586, 1674, 585, 40200, 9116, 2257, 584];
-let signals: number[][] = [[]];
-let signalNr: number = 0;
-let mapping: number[] = [];
 
-let pin = AnalogPin.P0
-pins.analogWritePin(pin, 0);
-pins.analogSetPeriod(pin, 26);
+// --- Settings ---
+let irDiodePin = AnalogPin.P0
+let irReceiverPin = DigitalPin.P2
+// ---
 
-function enableIrMarkSpaceDetection(pin: DigitalPin) {
-    pins.setPull(pin, PinPullMode.PullNone);
+let signals: number[][] = [];
+let currentSignal: number[] = [];
+
+pins.analogWritePin(irDiodePin, 0);
+pins.analogSetPeriod(irDiodePin, 26);
+
+function enableIrMarkSpaceDetection(irDiodePin: DigitalPin) {
+    pins.setPull(irDiodePin, PinPullMode.PullNone);
 
     let mark = 0;
     let space = 0;
 
-    pins.onPulsed(pin, PulseValue.Low, () => {
-        // HIGH, see https://github.com/microsoft/pxt-microbit/issues/1416
+    pins.onPulsed(irDiodePin, PulseValue.Low, () => {
         mark = pins.pulseDuration();
-        signals[signalNr].push(mark)
+        currentSignal.push(mark)
     });
 
-    pins.onPulsed(pin, PulseValue.High, () => {
-        // LOW
+    pins.onPulsed(irDiodePin, PulseValue.High, () => {
         space = pins.pulseDuration();
-        if (space > 500000){
-            signals[signalNr] = []
+        if (space > 100000){
+            currentSignal = []
         } else {
-            signals[signalNr].push(space)
+            currentSignal.push(space)
         }
     });
 }
 
-enableIrMarkSpaceDetection(DigitalPin.P2)
+enableIrMarkSpaceDetection(irReceiverPin)
 
 input.onButtonPressed(Button.AB, function () {
-    serial.writeNumbers([signals.length])
+    // Show stored signals.
+    serial.writeString("Recorded signals:\n")
+
+    for (let i = 0; i < signals.length; i++) {
+        serial.writeString(i + "\n")
+        serial.writeNumbers(signals[i])
+    }
+
+    // Send stored signals.
     for (let signal of signals){
-        serial.writeNumbers([1])
-        serial.writeNumbers(signal)
         send(signal)
         basic.pause(1000)
     }
 })
 
+// Send the last received signal.
 input.onButtonPressed(Button.A, function () {
-    send(signals[signalNr])
+    send(currentSignal)
 })
 
+// Store the last received signal.
 input.onButtonPressed(Button.B, function() {
-    signals.push([])
-    signalNr += 1
+    signals.push(currentSignal)
 })
 
 function send(signal: number[]){
     let isHight = false;
     for (let time of signal) {
         if (isHight) {
-            pins.analogWritePin(pin, 1);
+            pins.analogWritePin(irDiodePin, 1);
         } else {
-            pins.analogWritePin(pin, 511);
+            pins.analogWritePin(irDiodePin, 511);
         }
 
         control.waitMicros(time);
         isHight = !isHight
     }
-    pins.analogWritePin(pin, 0);
+    pins.analogWritePin(irDiodePin, 0);
+}
+
+// --- Processing the last received signal. ---
+
+let lastSignal = '';
+let lastSignalCheckNr = 0;
+
+basic.forever(() => {
+    let currentSignalKey = currentSignal.join('_');
+    if (currentSignalKey != ''){
+        if (lastSignal != currentSignalKey) {
+            lastSignal = currentSignalKey
+            lastSignalCheckNr = 0
+        } else {
+            lastSignalCheckNr++;
+
+            if (lastSignalCheckNr == 1) {
+                control.runInBackground(() => {
+                    processSignal()
+                })
+            }
+        }
+    }
+})
+
+// Checks if the current signal matches a stored signal.
+
+function processSignal(){
+    for (let i = 0; i < signals.length; i++) {
+        let signal = signals[i]
+
+        let test = true;
+        for (let n = 0; n < signal.length; n++){
+            if (currentSignal[n] > signal[n] + 50 || currentSignal[n] < signal[n] - 50){
+                test = false;
+                break;
+            }
+        }
+
+        if (test){
+            basic.showNumber(i)
+        } else {
+            continue;
+        }
+    }
 }
