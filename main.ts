7164fcf57f21246687287c75cdf4a17ccf2de385
Adam Orczyk <adam.orczyk@gmail.com>
2022-09-27 20:40:06 +0200

    Update main.ts

--- main.ts
+++ main.ts
@@ -2,10 +2,12 @@
 // --- Settings ---
 let irDiodePin = AnalogPin.P0
 let irReceiverPin = DigitalPin.P2
+let signalPartTolerance = 50
 // ---
 
 let signals: number[][] = [];
 let currentSignal: number[] = [];
+let receivedSignal: number[] = [];
 
 pins.analogWritePin(irDiodePin, 0);
 pins.analogSetPeriod(irDiodePin, 26);
@@ -18,15 +20,15 @@ function enableIrMarkSpaceDetection(irDiodePin: DigitalPin) {
 
     pins.onPulsed(irDiodePin, PulseValue.Low, () => {
         mark = pins.pulseDuration();
-        currentSignal.push(mark)
+        receivedSignal.push(mark)
     });
 
     pins.onPulsed(irDiodePin, PulseValue.High, () => {
         space = pins.pulseDuration();
         if (space > 100000){
-            currentSignal = []
+            receivedSignal = []
         } else {
-            currentSignal.push(space)
+            receivedSignal.push(space)
         }
     });
 }
@@ -75,37 +77,49 @@ function send(signal: number[]){
 }
 
 // --- Processing the last received signal. ---
-
-let lastSignal = '';
-let lastSignalCheckNr = 0;
+let lastReceivedSignalKey = '';
+let signalCounter = 0;
 
 basic.forever(() => {
-    let currentSignalKey = currentSignal.join('_');
-    if (currentSignalKey != ''){
-        if (lastSignal != currentSignalKey) {
-            lastSignal = currentSignalKey
-            lastSignalCheckNr = 0
+    if (receivedSignal.length){
+        let receivedSignalKey = receivedSignal.join('_')
+        if (lastReceivedSignalKey != receivedSignalKey){
+            lastReceivedSignalKey = receivedSignalKey
         } else {
-            lastSignalCheckNr++;
+            currentSignal = JSON.parse(JSON.stringify(receivedSignal))
 
-            if (lastSignalCheckNr == 1) {
-                control.runInBackground(() => {
-                    processSignal()
-                })
-            }
+
+            lastReceivedSignalKey = ''
+            control.runInBackground(() => {
+                processSignal(signals, currentSignal)
+            })
+            receivedSignal = []
+
+            // Debug
+            // serial.writeString('\n> ' + signalCounter + '\n')
+            // signalCounter++
+            // serial.writeNumbers(currentSignal)
         }
+
     }
 })
 
 // Checks if the current signal matches a stored signal.
 
-function processSignal(){
-    for (let i = 0; i < signals.length; i++) {
-        let signal = signals[i]
+function processSignal(patterns: number[][], signal: number[]){
+    if (!signal.length){
+        return false;
+    }
+    
+    for (let i = 0; i < patterns.length; i++) {
+        let pattern = patterns[i]
+        if (!pattern.length){
+            continue
+        }
 
         let test = true;
-        for (let n = 0; n < signal.length; n++){
-            if (currentSignal[n] > signal[n] + 50 || currentSignal[n] < signal[n] - 50){
+        for (let n = 0; n < pattern.length; n++){
+            if (signal[n] > pattern[n] + signalPartTolerance || signal[n] < pattern[n] - signalPartTolerance){
                 test = false;
                 break;
             }
@@ -113,25 +127,28 @@ function processSignal(){
 
         if (test){
             runOnSignal(i)
+            return true
         } else {
             continue;
         }
     }
+
+    return false
 }
 
 // Runs the command when the recorded signal appears.
 
 function runOnSignal(signalId: number){
-    basic.showNumber(signalId)
+    // basic.showNumber(signalId)
 
     if (signalId == 0){
-        basic.clearScreen()
-        music.playSoundEffect(music.createSoundEffect(WaveShape.Square, 1600, 1, 255, 0, 300, SoundExpressionEffect.None, InterpolationCurve.Curve), SoundExpressionPlayMode.UntilDone)
+        led.plot(2,2)
     } else if (signalId == 1) {
         basic.showIcon(IconNames.Heart)
-        music.playSoundEffect(music.createSoundEffect(WaveShape.Noise, 54, 54, 255, 0, 500, SoundExpressionEffect.None, InterpolationCurve.Linear), SoundExpressionPlayMode.UntilDone)
     } else if (signalId == 2) {
         basic.showIcon(IconNames.House)
-        music.playSoundEffect(music.createSoundEffect(WaveShape.Square, 400, 600, 255, 0, 100, SoundExpressionEffect.Warble, InterpolationCurve.Linear), SoundExpressionPlayMode.UntilDone)
     }
+
+    basic.pause(500)
+    basic.clearScreen()
 }
